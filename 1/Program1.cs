dbf5c81f1eed95acf16356ffdffbd72c2c297773
NIKITA <>
2023-02-24 16:37:56 +0300

    second task is complite

--- 1/Program1.cs
+++ 1/Program1.cs
@@ -1,8 +1,12 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
 int ArrLen = 10;
 int[] Numbers = new int[ArrLen];
+int notevensum = 0;
 void FillArray(int[] Numbers)
 {
     int evensum = 0;
@@ -14,9 +18,12 @@ void FillArray(int[] Numbers)
         Console.Write(Numbers[index] + " ");
           if (Numbers[index] % 2 == 0)
               {evensum = evensum + 1;}
+        if (index % 2 == 1)
+        {notevensum = notevensum + Numbers[index];}
+
         index++;
     }
-   Console.Write("Even number " + evensum); 
+   Console.Write("Even number " + evensum + "Sum of numbers at odd indices = " + notevensum); 
 }
 
 FillArray(Numbers);
